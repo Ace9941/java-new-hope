ff5c3ee64159e3006b7707f5c2518cdf82b46e6c
Vadim <ace9941@gmail.com>
2020-11-15 13:54:35 +0200

    Initial commit

---
+++ src/test/java/MyfFirstTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+public class MyfFirstTest {
+    private String name = "Lord";
+
+    @Test
+    public void firstTest() {
+        System.out.println("Hello, corona!");
+
+        int sum = sumTwoDigits(15 ,45);
+        System.out.println("And the sum is: " + sum);
+
+        System.out.println(sumTwoDigits(9, 13));
+        System.out.println(sumTwoDigits(911, 100500));
+        System.out.println(name);
+    }
+
+    private int sumTwoDigits(int a, int b) {
+        System.out.println(name);
+        int c = a + b;
+        return c;
+    }
+}
